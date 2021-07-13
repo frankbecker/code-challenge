046df65902631be794162058e9e95350d0cd2095
frankbecker <Frank Becker>
2021-07-12 21:16:28 -0400

    Cleaning up the code a bit #3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.scss";
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
